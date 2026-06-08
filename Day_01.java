eedaf559c7dabdd75b9c73e71d3d0293e5c3bc6f
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-08 12:19:14 +0530

    Implement Two Sum solution in Day_01.java

---
+++ Day_01.java
@@ -0,0 +1,12 @@
+// Two Sum 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int leftsum = target - nums[i];
+            if(mpp.containsKey(leftsum)) return new int[]{mpp.get(leftsum),i};
+            mpp.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
