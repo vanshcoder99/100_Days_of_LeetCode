9ef5ef83a768b2d067e328159f80cbd61f7c0fe6
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-10 21:10:42 +0530

    Add merge function for merging sorted arrays
    Implement merge function to merge two sorted arrays.

--- Day_03.java
+++ Day_03.java
@@ -48,3 +48,29 @@ class Solution {
         return cnt;
     }
 }
+
+
+// Merge Sorted Array
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(n == 0) return;
+        int i = m-1, j = n - 1, k = m + n - 1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                nums1[i] = 0;
+                k--;
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+        while(j>=0 && k>=0){
+            nums1[k--] = nums2[j--];
+        }
+
+    }
+}
