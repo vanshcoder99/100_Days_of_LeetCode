3379a2b4c6bd56557a065c5886c4840de6af6532
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-11 00:24:49 +0530

    Implement next permutation algorithm

--- Day_03.java
+++ Day_03.java
@@ -74,3 +74,45 @@ class Solution {
 
     }
 }
+
+
+// Next Permutation
+class Solution {
+    public void reverse(int[] nums,int i, int j){
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int ind = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        if(ind == -1){
+            reverse(nums,0,n-1);
+            return;
+        }
+
+        int i = n-1;
+        while(i>ind){
+            if(nums[ind]<nums[i]){
+                int temp = nums[ind];
+                nums[ind] = nums[i];
+                nums[i] = temp;
+                break;
+            }
+            i--;
+        }
+
+        reverse(nums,ind+1,n-1);
+
+    }
+}
