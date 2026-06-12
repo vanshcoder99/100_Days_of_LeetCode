1f3b9512f03fd98d294445c53fb2202577604162
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-12 20:40:55 +0530

    Implement power function with myPow method

--- Day_05.java
+++ Day_05.java
@@ -24,3 +24,18 @@ class Solution {
         return image;
     }
 }
+
+// Pow(x, n)
+class Solution {
+    public double pow(double x, long n){
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n % 2 == 0) return pow(x*x,n/2);
+        return x * pow(x,n-1);
+    }
+    public double myPow(double x, int n) {
+        long num = n;
+        if(num<0) return 1/pow(x,-num);
+        return pow(x,num);
+    }
+}
