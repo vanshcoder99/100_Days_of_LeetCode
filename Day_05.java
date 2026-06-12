f554f4424c99087d753bf0f2b3029f8d015ca216
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-12 16:23:27 +0530

    Implement flood fill algorithm in Day_05.java

---
+++ Day_05.java
@@ -0,0 +1,26 @@
+// Flood Fill
+class Solution {
+    public void dfs(int[][] image, int sr, int sc, int color,int val) {
+        image[sr][sc] = color;
+        int n = image.length;
+        int m = image[0].length;
+        for(int i=0;i<2;i++){
+            int nrow = (i%2==0) ? sr + 1 : sr - 1;
+            int ncol = (i%2==0) ? sc + 1 : sc - 1;
+            if(nrow >= 0 && nrow < n && image[nrow][sc] == val){
+                dfs(image,nrow,sc,color,val);
+            }
+            if(ncol>= 0 && ncol < m && image[sr][ncol] == val){
+                dfs(image,sr,ncol,color,val);
+            }
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
+        int val = image[sr][sc];
+        if(val == color) return image;
+        dfs(image,sr,sc,color,val);
+        return image;
+    }
+}
