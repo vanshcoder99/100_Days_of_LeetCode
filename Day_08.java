46ec10c2d1732e9045ff9e9b30c357683ab509da
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-15 21:17:46 +0530

    Add method to find duplicates in an array

--- Day_08.java
+++ Day_08.java
@@ -14,3 +14,19 @@ class Solution {
         return ans;
     }
 }
+
+
+// Find All Duplicates in an Array
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        if(n == 1) return ans;
+        for(int i=0;i<n;i++){
+            int idx = Math.abs(nums[i]) - 1;
+            if(nums[idx] < 0) ans.add(Math.abs(nums[i]));
+            else nums[idx] = - nums[idx];
+        }
+        return ans;
+    }
+}
