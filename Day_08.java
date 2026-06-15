ab87181ddac0c95268d28f29e3bc9ae5c5854421
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-15 21:39:14 +0530

    Add DFS solution for counting enclaves
    Implement DFS to count number of enclaves in grid.

--- Day_08.java
+++ Day_08.java
@@ -30,3 +30,42 @@ class Solution {
         return ans;
     }
 }
+
+
+// Number of Enclaves
+class Solution {
+    public void dfs(int row, int col, int[] delrow, int[] delcol, int[][] grid){
+        int n = grid.length;
+        int m = grid[0].length;
+        grid[row][col] = 0;
+        for(int i=0;i<4;i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1){
+                dfs(nrow,ncol,delrow,delcol,grid);
+            }
+        }
+    }
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int cnt = 0;
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i == 0 || j == 0 || i == n-1 || j == m-1) && grid[i][j] == 1){
+                    dfs(i,j,delrow,delcol,grid);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
