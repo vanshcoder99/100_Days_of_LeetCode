2af7a059e50bb5cb2ae8373594f7bc328e201c36
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-17 21:11:25 +0530

    Implement image rotation algorithm

---
+++ Day_10.java
@@ -0,0 +1,30 @@
+// Rotate Image
+class Solution {
+    public void reverse(int[] arr, int n){
+        int i = 0, j = n - 1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(i!=j){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i],n);
+        }
+
+    }
+}
