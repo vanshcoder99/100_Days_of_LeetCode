9cb2d5ec3b17109963ff2dc8bbde9016ded1d8ce
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-19 23:58:37 +0530

    Add function to find missing and repeating numbers
    Implemented a function to find missing and repeating numbers in an array.

--- Day_12.java
+++ Day_12.java
@@ -26,3 +26,59 @@ class Solution {
         return res;
     }
 }
+
+// Missing And Repeating  --> GFG PRACTICE PROBLEM
+import java.util.*;
+
+class Solution {
+    // Function to find repeating and missing numbers
+    public int[] findMissingRepeatingNumbers(int[] nums) {
+        
+        // Size of the array
+        long n = nums.length;
+
+        // Sum of first n natural numbers
+        long SN = (n * (n + 1)) / 2;
+
+        // Sum of squares of first n natural numbers
+        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        /* Calculate actual sum (S) and sum 
+           of squares (S2) of array elements */
+        long S = 0, S2 = 0;
+        for (int i = 0; i < n; i++) {
+            S += nums[i];
+            S2 += (long) nums[i] * (long) nums[i];
+        }
+
+        // Compute the difference values
+        long val1 = S - SN;
+
+        // S2 - S2n = X^2 - Y^2
+        long val2 = S2 - S2N;
+
+        // Calculate X + Y using X + Y = (X^2 - Y^2) / (X - Y)
+        val2 = val2 / val1;
+
+        /* Calculate X and Y from X + Y and X - Y
+           X = ((X + Y) + (X - Y)) / 2
+           Y = X - (X - Y) */
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+
+        // Return the results as [repeating, missing]
+        return new int[]{(int) x, (int) y};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 2, 5, 4, 6, 7, 5};
+
+        // Create an instance of Solution class
+        Solution sol = new Solution();
+
+        int[] result = sol.findMissingRepeatingNumbers(nums);
+
+        // Print the repeating and missing numbers found
+        System.out.printf("The repeating and missing numbers are: {%d, %d}\n", result[0], result[1]);
+    }
+}
