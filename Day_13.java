e4574905df7a57e2892671bdefc59201758162b1
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-20 19:36:33 +0530

    Implement fourSum method for finding quadruplets

--- Day_13.java
+++ Day_13.java
@@ -47,3 +47,35 @@ class Solution {
         return ans;
     }
 }
+
+
+// 4sum
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<n-3;i++){
+            if(i!=0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j!=i+1 && nums[j] == nums[j-1]) continue;
+                int l = j + 1;
+                int k = n - 1;
+                while(l<k){
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target){
+                        List<Integer> list = Arrays.asList(nums[i],nums[j],nums[l] ,nums[k]);
+                        ans.add(list);
+                        l++;
+                        k--;
+                        while(l<k && nums[l]==nums[l-1]) l++;
+                        while(l<k && nums[k]==nums[k+1]) k--;
+                    }
+                    else if(sum>target) k--;
+                    else l++;
+                }
+            }
+        }
+        return ans;
+    }
+}
