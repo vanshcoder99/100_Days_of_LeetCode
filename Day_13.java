b2ad5433864a822490299922694ea9bad605eb6c
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-20 17:32:18 +0530

    Implement 3Sum algorithm in Day_13.java

--- Day_13.java
+++ Day_13.java
@@ -19,3 +19,31 @@ class Solution {
         return new int[]{(int)x,(int)y};
     }
 }
+
+// 3Sum
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+            if(i!=0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum > 0) k--;
+                else if(sum<0) j++;
+                else{
+                    List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(list);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
