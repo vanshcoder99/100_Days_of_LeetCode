db06b9bb999c0761ee82ba2789f49a3739dc9a8a
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-29 23:26:28 +0530

    Add countPrimes method to count prime numbers
    Implement countPrimes method to count prime numbers less than n.

--- Day_22.java
+++ Day_22.java
@@ -80,3 +80,24 @@ class Solution {
         return false;
     }
 }
+
+
+
+// Count Primes
+class Solution {
+    public int countPrimes(int n) {
+        int cnt = 0;
+        boolean[] prime = new boolean[n];
+        for(int i=2;i*i<n;i++){
+            if(!prime[i]){
+                for(int j=i*i;j<n;j+=i){
+                    prime[j] = true;
+                }
+            }
+        }
+        for(int i=2;i<n;i++){
+            if(!prime[i]) cnt++;
+        }
+        return cnt;
+    }
+} 
