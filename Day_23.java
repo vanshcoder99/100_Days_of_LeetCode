27027281cc2cbf886e686630e0f09942e3eea692
Vansh Nagpal <nagpalv931@gmail.com>
2026-06-30 13:05:02 +0530

    Implement cycle detection in 2D grid

--- Day_23.java
+++ Day_23.java
@@ -31,3 +31,41 @@ class Solution {
         return st.size();
     }
 }
+
+
+
+// Detect Cycles in 2D Grid
+class Solution {
+    public boolean dfs(int r,int c,char[][] grid,boolean[][] vis,int pr,int pc) {
+        int n = grid.length;
+        int m = grid[0].length;
+        vis[r][c] = true;
+        char val = grid[r][c];
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nr = r + delrow[i];
+            int nc = c + delcol[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == val && !vis[nr][nc]){
+                if(dfs(nr,nc,grid,vis,r,c)) return true;
+            }
+            else if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc] && grid[nr][nc] == val && (pr != -1 && pc != -1) && (pr != nr || pc != nc)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean containsCycle(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]){
+                    if(dfs(i,j,grid,vis,-1,-1)) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
