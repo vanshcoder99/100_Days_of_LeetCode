45e8ca81c8310a9cedbfcaf5952cb82c5d6cbec8
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-01 18:52:16 +0530

    Add course schedule completion method
    Implemented a method to determine if all courses can be finished based on prerequisites using a graph approach.

--- Day_24.java
+++ Day_24.java
@@ -138,3 +138,40 @@ class Solution {
         return ans;
     }
 }
+
+
+// Course Scheduleclass Solution {
+    public boolean canFinish(int numc, int[][] pr) {
+        int n = pr.length;
+        if(n == 0) return true;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numc;i++){
+            adj.add(new ArrayList<>());
+        }
+        int[] indeg = new int[numc];
+        
+        for(var it : pr){
+            adj.get(it[1]).add(it[0]);
+            indeg[it[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numc;i++){
+            if(indeg[i] == 0) q.offer(i); 
+        }
+
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            cnt++;
+            for(var it : adj.get(node)){
+                indeg[it]--;
+                if(indeg[it] == 0) q.offer(it);
+            }
+        }
+
+        if(cnt == numc) return true;
+        return false;
+
+    }
+}
