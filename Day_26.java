1fa9324db93d815d8385b0582ce55dcc82ce0fe6
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-03 23:11:36 +0530

    Implement maximum subarray solution

---
+++ Day_26.java
@@ -0,0 +1,13 @@
+// Maximum Subarray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxi = Integer.MIN_VALUE, sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxi = Math.max(maxi,sum);
+            if(sum<0) sum = 0;
+        }
+        return maxi;
+    }
+}
