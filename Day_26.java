dc41bae892ec73b290fa1bd3922988cac6b3b6b2
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-04 23:37:36 +0530

    Add containsNearbyDuplicate method to Solution class

--- Day_26.java
+++ Day_26.java
@@ -11,3 +11,20 @@ class Solution {
         return maxi;
     }
 }
+
+
+// Contains Duplicate II
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(mpp.containsKey(nums[i])){
+                int val = mpp.get(nums[i]);
+                if(Math.abs(val-i) <= k) return true;
+            }
+            mpp.put(nums[i],i);
+        }
+        return false;
+    }
+}
