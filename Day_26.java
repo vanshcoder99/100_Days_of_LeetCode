d152ba47f042f681108357379f802710feb6cd8d
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-05 23:22:26 +0530

    Implement subarraySum method for counting subarrays

--- Day_26.java
+++ Day_26.java
@@ -28,3 +28,21 @@ class Solution {
         return false;
     }
 }
+
+
+// 
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0, presum = 0;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        mpp.put(0,1);
+        for(int i=0;i<n;i++){
+            presum+=nums[i];
+            int remove = presum - k;
+            cnt+=mpp.getOrDefault(remove,0);
+            mpp.put(presum,mpp.getOrDefault(presum,0) + 1);
+        }
+    return cnt;
+    }
+}
