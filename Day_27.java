49953cf0d139b9d6ed96119c8095c55e690845df
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-07 00:15:59 +0530

    Implement Trapping Rain Water algorithm

--- Day_27.java
+++ Day_27.java
@@ -19,3 +19,27 @@ class Solution {
         return maxi;
     }
 }
+
+
+
+// Trapping Rain Water
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int l = 0, r = n - 1, lmax = 0, rmax = 0;
+        int total = 0;
+        while(l<r){
+            if(height[l]>=height[r]){
+                if(height[r]>rmax) rmax = height[r];
+                else total += (rmax - height[r]);
+                r--;
+            }
+            else{
+                if(height[l]>lmax) lmax = height[l];
+                else total+=(lmax-height[l]);
+                l++;
+            }
+        }
+        return total;
+    }
+}
