15b8ff9bd6aa2dd91ae49f13f1c652638b350991
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-07 14:57:56 +0530

    Add subarray sum equals k method
    Implemented a method to count subarrays that sum to a given value k using a hashmap for prefix sums.

--- Day_28.java
+++ Day_28.java
@@ -77,3 +77,21 @@ class Solution {
         return maxlen;
     }
 }
+
+
+
+// Subarray Sum Equals K
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0, presum = 0;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        mpp.put(0,1);
+        for(int i=0;i<n;i++){
+            presum+=nums[i];
+            if(mpp.containsKey(presum-k)) cnt+=mpp.get(presum-k);
+            mpp.put(presum,mpp.getOrDefault(presum,0)+1);
+        }
+        return cnt;
+    }
+}
