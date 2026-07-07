c8d2720017bfc4f821da81319b8e96c01d4c156d
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-08 00:51:09 +0530

    Add shortest path algorithm for Directed Acyclic Graph
    Implemented a DFS-based solution to find the shortest path in a Directed Acyclic Graph (DAG). The method initializes adjacency lists, performs DFS for topological sorting, and calculates shortest paths using edge weights.

---
+++ Day_29.java
@@ -0,0 +1,55 @@
+// Shortest path in Directed Acyclic Graph
+class Pair{
+    int first, second;
+    Pair(int f, int s){
+        this.first = f;
+        this.second = s;
+    }
+}
+class Solution {
+    public void dfs(int node, boolean[] vis,Stack<Integer> st,List<List<Pair>> adj){
+        vis[node] = true;
+        for(var it : adj.get(node)){
+            int v = it.first;
+            if(!vis[v]){
+                dfs(v,vis,st,adj);
+            }
+        }
+        st.push(node);
+    }
+    public int[] shortestPath(int N, int M, int[][] edges) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(var it : edges){
+            adj.get(it[0]).add(new Pair(it[1],it[2]));
+        }
+        Stack<Integer> st = new Stack<>();
+        boolean[] vis = new boolean[N];
+        for(int i=0;i<N;i++){
+            if(!vis[i]){
+                dfs(i,vis,st,adj);
+            }
+        }
+
+        int[] dis = new int[N];
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        dis[0] = 0;
+        while(!st.isEmpty()){
+            int node = st.pop();
+            for(var it : adj.get(node)){
+                int v =  it.first;
+                int wt =  it.second;
+                if(dis[node] != Integer.MAX_VALUE && dis[node] + wt < dis[v]){
+                    dis[v] = dis[node] + wt;
+                }
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            if(dis[i] == Integer.MAX_VALUE) dis[i] = -1;
+        }
+        return dis;
+    }
+}
