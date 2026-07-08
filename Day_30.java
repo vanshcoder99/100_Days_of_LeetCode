2c3277a1070556353517e28623f3e53ae81833ed
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-09 02:14:57 +0530

    Add method to find maximum average subarray
    Implement findMaxAverage method to calculate maximum average of subarray.

--- Day_30.java
+++ Day_30.java
@@ -14,3 +14,22 @@ class Solution {
         return cnt;
     }
 }
+
+
+
+// Maximum Average Subarray I
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0, maxsum = Integer.MIN_VALUE;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        maxsum = Math.max(sum,maxsum); 
+        for(int i=k;i<n;i++){
+            sum = sum - nums[i-k] + nums[i];
+            maxsum = Math.max(sum,maxsum); 
+        }
+        return (double)maxsum/k;
+    }
+}
