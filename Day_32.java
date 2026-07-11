cf4cbc328594b28a30189cfc3fcbed07d1126767
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-11 15:39:38 +0530

    Implement beautifulSubarrays method in Solution class
    Added a method to count the number of beautiful subarrays using XOR.

--- Day_32.java
+++ Day_32.java
@@ -14,3 +14,21 @@ class Solution {
         return (minlen != Integer.MAX_VALUE) ? minlen : -1;
     }
 }
+
+
+
+// Count the Number of Beautiful Subarrays
+class Solution {
+    public long beautifulSubarrays(int[] nums) {
+        int n = nums.length,prexor = 0;
+        long cnt = 0;
+        Map<Integer,Integer> mpp = new HashMap<>();
+        mpp.put(0,1);
+        for(int i=0;i<n;i++){
+            prexor = prexor ^ nums[i];
+            if(mpp.containsKey(prexor)) cnt += mpp.get(prexor);
+            mpp.put(prexor,mpp.getOrDefault(prexor,0)+1);
+        }
+        return cnt;
+    }
+}
