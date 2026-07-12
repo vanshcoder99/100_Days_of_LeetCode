947d1258d10c7a1959c4d35f8ffc6275543aadb9
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-12 18:48:28 +0530

    Add arrayPairSum method to calculate pair sum

--- Day_33.java
+++ Day_33.java
@@ -59,3 +59,17 @@ class Solution {
         return maxlen;
     }
 }
+
+
+
+// class Solution {
+    public int arrayPairSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i=0;i<n;i+=2){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+}
