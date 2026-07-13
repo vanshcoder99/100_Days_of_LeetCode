2ff28bd144accf0b29835fc483d6c37f9c0a493b
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-13 14:41:09 +0530

    Add player-trainer matching functionality
    Implemented a method to match players with trainers by sorting both arrays and using a two-pointer approach.

--- Day_34.java
+++ Day_34.java
@@ -12,3 +12,20 @@ class Solution {
         return i;
     }
 }
+
+
+
+// Maximum Matching of Players With Trainers
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        int n = players.length, m = trainers.length;
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i=0, j=0;
+        while(i<n && j<m){
+            if(players[i]<=trainers[j]) i++;
+            j++;
+        }
+        return i;
+    }
+}
