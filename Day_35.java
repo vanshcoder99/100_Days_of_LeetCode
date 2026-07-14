907d1364a87b0bcadcd0cb1f68b8caa70db920f7
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-14 13:35:37 +0530

    Add maxMeetings method to Solution class
    Implemented a solution to find the maximum number of meetings that can be scheduled in one room based on start and finish times.

--- Day_35.java
+++ Day_35.java
@@ -26,3 +26,28 @@ class Solution {
         return new ArrayList<>(Arrays.asList(c,p));
     }
 }
+
+
+
+// Maximum Meetings in One Room
+class Solution {
+    public ArrayList<Integer> maxMeetings(int[] s, int[] f) {
+        int n = s.length;
+        int[][] arr = new int[n][3];
+        for(int i=0;i<n;i++){
+            arr[i] = new int[]{s[i],f[i],i+1};
+        }
+        Arrays.sort(arr,(a,b) -> a[1] - b[1]);
+        ArrayList<Integer> list = new ArrayList<>();
+        int ed = arr[0][1];
+        list.add(arr[0][2]);
+        for(int i=1;i<n;i++){
+            if(arr[i][0] > ed){
+                list.add(arr[i][2]);
+                ed = arr[i][1];
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
