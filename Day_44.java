faa576604b06edc6e60b974dccd6b82fe62edd46
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-23 15:44:20 +0530

    Add repeatedSubstringPattern method to Solution class
    Implement repeated substring pattern detection in Java.

--- Day_44.java
+++ Day_44.java
@@ -23,3 +23,33 @@ class Solution {
         return s.substring(0,lps[n-1]);
     }
 }
+
+
+
+// Repeated Substring Pattern
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        int len = 0, i = 1;
+        while(i<n){
+            if(s.charAt(len) == s.charAt(i)){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len != 0){
+                    len = lps[len-1];
+                }
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        int rlen = n - lps[n-1];
+        if(rlen == n) return false;
+        return (n % rlen == 0) ? true : false;
+    }
+}
