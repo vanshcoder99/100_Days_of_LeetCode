231d68066aecd2bae5e422a1661f4c60c403b3bf
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-27 00:40:24 +0530

    Implement totalFruit method in Solution class
    Added a new class 'Solution' with a method to calculate the maximum number of fruits that can be collected in two baskets.

--- Day_46.java
+++ Day_46.java
@@ -26,3 +26,27 @@ class Solution {
         return rev.substring(0,rev.length()-lps[size-1]) + s;
     }
 }
+
+
+
+// Fruit Into Baskets
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        if(n == 1) return 1;
+        Map<Integer,Integer> mpp = new HashMap<>();
+        int l = 0,maxcnt = 0;
+        for(int r=0;r<n;r++){
+            mpp.put(fruits[r],mpp.getOrDefault(fruits[r],0)+1);
+            if(mpp.size()>2){
+                int val = fruits[l];
+                int freq = mpp.get(val);
+                if(freq - 1 == 0) mpp.remove(val);
+                else mpp.put(val,freq-1);
+                l++;
+            }
+            if(mpp.size()<=2) maxcnt = Math.max(maxcnt,r-l+1);
+        }
+        return maxcnt;
+    }
+}
