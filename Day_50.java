9df336628d97b47f2a33a4a27dede7a40bde2fe6
Vansh Nagpal <nagpalv931@gmail.com>
2026-07-30 15:24:44 +0530

    Implement K Closest Points to Origin solution

--- Day_50.java
+++ Day_50.java
@@ -58,3 +58,24 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+// K Closest Points to Origin
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int n = points.length;
+        int[][] ans = new int[k][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0]*a[0]+a[1]*a[1],b[0]*b[0] + b[1]*b[1]));
+        
+        for(int i=0;i<n;i++){
+            pq.offer(points[i]);
+        }
+        int i = 0;
+        while(i<k && !pq.isEmpty()){
+            ans[i] = pq.poll();
+            i++;
+        }
+        return ans;
+    }
+}
