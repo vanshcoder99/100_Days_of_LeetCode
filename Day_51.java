4afb4da318c2ecb1ffe17dfa0496b53c43a17147
Vansh Nagpal <nagpalv931@gmail.com>
2026-08-02 16:47:49 +0530

    Add Spiral Matrix II implementation

--- Day_51.java
+++ Day_51.java
@@ -31,3 +31,39 @@ class Solution {
         return ans;
     }
 }
+
+// Spiral Matrix II
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int left = 0, top = 0, right = n - 1, bottom = n - 1, num = 1;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                matrix[top][i] = num;
+                num++;
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                matrix[i][right] = num;
+                num++;
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    matrix[bottom][i] = num;
+                    num++;
+                }
+                bottom--;
+            }
+
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    matrix[i][left] = num;
+                    num++;
+                }
+                left++;
+            }
+        }
+        return matrix;
+    }
+}
