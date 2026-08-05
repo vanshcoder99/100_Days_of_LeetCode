48e1d41a2ae267da76b2a40dcea9e4f4a037748e
Vansh Nagpal <nagpalv931@gmail.com>
2026-08-05 23:42:09 +0530

    Add method to count hills and valleys in an array

--- Day_54.java
+++ Day_54.java
@@ -16,3 +16,28 @@ class Solution {
         return list;
     }
 }
+
+// Count Hills and Valleys in an Array
+class Solution {
+    public int countHillValley(int[] nums) {
+        int n = nums.length;
+        int i = 1,cnt=0;
+        while(i<n-1){
+            int curr = nums[i];
+            int prev = nums[i-1];
+            if(curr == prev){
+                i++;
+                continue;
+            }
+            int j = i+1;
+            while(j<n-1){
+                if(curr == nums[j]) j++;
+                else break;
+            }
+            i = j;
+            if((prev>curr && nums[j]>curr) || (prev<curr && nums[j]<curr)) cnt++;
+        }
+
+        return cnt;
+    }
+}
