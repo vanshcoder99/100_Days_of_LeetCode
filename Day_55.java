4ed8fb5cef708f49e0134259229638b27f210d44
Vansh Nagpal <nagpalv931@gmail.com>
2026-08-09 19:19:11 +0530

    Add shortestPath method using Dijkstra's algorithm
    Implemented Dijkstra's algorithm to find the shortest path in a graph.

--- Day_55.java
+++ Day_55.java
@@ -34,3 +34,75 @@ class Solution {
         return dist[dest];
     }
 }
+
+
+
+// Print Shortest Path
+class Pair{
+    int node;
+    int wt;
+    Pair(int nd,int w){
+        node = nd;
+        wt = w;
+    }
+}
+class Solution {
+    public List<Integer> shortestPath(int n, int m, int[][] edges) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(var it : edges){
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            adj.get(u).add(new Pair(v,w));
+            adj.get(v).add(new Pair(u,w));
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
+        int[] dist = new int[n+1];
+        int[] parent = new int[n+1];
+
+        Arrays.fill(dist,(int)1e9);
+        dist[1] = 0;
+        pq.offer(new int[]{0,1});
+
+        for(int i=1;i<=n;i++){
+            parent[i] = 1;
+        }
+
+        while(!pq.isEmpty()){
+            int[] arr = pq.poll();
+            int dis = arr[0];
+            int node = arr[1];
+            if(dis>dist[node]) continue;
+            for(Pair it : adj.get(node)){
+                int adjdis = it.wt;
+                int adjnode = it.node;
+                if(dis + adjdis < dist[adjnode]){
+                    dist[adjnode] = dis + adjdis;
+                    pq.offer(new int[]{dist[adjnode],adjnode});
+                    parent[adjnode] = node;
+                }
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        if(dist[n] == (int)1e9){
+            list.add(-1);
+            return list;
+        }
+        int node = n;
+        while(parent[node] != node){
+            list.add(node);
+            node = parent[node];
+        }
+
+        list.add(1);
+        list.add(dist[n]);
+        Collections.reverse(list);
+        return list;
+    }
+}
+
+
+
