d52342199cf19eb5d3f69dc8cdef8b7559c22976
Vansh Nagpal <nagpalv931@gmail.com>
2026-08-14 00:52:04 +0530

    Add largestSumAfterKNegations method
    Implement function to maximize sum after K negations.

--- Day_59.java
+++ Day_59.java
@@ -17,3 +17,28 @@ class Solution {
         return cnt;
     }
 }
+
+
+
+// Maximize Sum Of Array After K Negations
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            if(k == 0) break;
+            if(nums[i] < 0){
+                nums[i] = - nums[i];
+                k--;
+            }
+        }
+        int sum = 0, min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            min = Math.min(min,nums[i]);
+        }
+        if(k % 2 == 0) return sum;
+        sum = sum - (2*min);
+        return sum;
+    }
+}
