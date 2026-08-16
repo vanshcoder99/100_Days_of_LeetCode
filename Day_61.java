db8a5631ecaa280f4d28df4038970807ea5bec65
Vansh Nagpal <nagpalv931@gmail.com>
2026-08-16 20:23:01 +0530

    Add productExceptSelf method in Solution class
    Implement product of array except self algorithm.

---
+++ Day_61.java
@@ -0,0 +1,19 @@
+// Product of Array Except Self
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[0] = 1;
+        int prod = nums[0];
+        for(int i=1;i<n;i++){
+            ans[i] = prod;
+            prod*=nums[i];
+        }
+        prod =  nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans[i]*=prod;
+            prod*=nums[i];
+        }
+        return ans;
+    }
+}
