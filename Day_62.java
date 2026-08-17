86d8a2301f8a196b6c8b207bf68afd83512149b0
Vansh Nagpal <nagpalv931@gmail.com>
2026-08-18 00:49:04 +0530

    Add minimumCost method to calculate candy costs
    Implement minimum cost calculation for candies with discount.

--- Day_62.java
+++ Day_62.java
@@ -39,3 +39,21 @@ class Solution {
         return 0;
     }
 }
+
+
+
+// Minimum Cost of Buying Candies With Discount
+class Solution {
+    public int minimumCost(int[] cost) {
+        int n = cost.length;
+        int sum = 0;
+        Arrays.sort(cost);
+        int j = 0;
+        for(int i=n-1;i>=0;i--){
+            j++;
+            if(j % 3 == 0) continue;
+            sum+=cost[i];
+        }
+        return sum;
+    }
+}
