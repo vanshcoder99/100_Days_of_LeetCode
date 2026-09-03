ff1e83051150337f7ce4a65ba558066499652fb8
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-04 00:38:22 +0530

    Implement Stickler Thief solution in Day_68.java

--- Day_68.java
+++ Day_68.java
@@ -14,3 +14,24 @@ class Solution {
         return prev;
     }
 }
+
+
+
+// Stickler Thief
+class Solution {
+    public int func(int n, int[] nums,int[] dp){
+        if(n<0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int sum1 = 0, sum2 = 0;
+        sum1+=nums[n];
+        sum1+=func(n-2,nums,dp);
+        sum2+=func(n-1,nums,dp);
+        return dp[n]=Math.max(sum1,sum2);
+    }
+    public int findMaxSum(int nums[]) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return func(n-1,nums,dp);
+    }
+}
