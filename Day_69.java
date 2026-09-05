dddd81007698c27725acb8333b69d13fac5ca5bc
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-06 02:44:35 +0530

    Implement unique paths calculation

--- Day_69.java
+++ Day_69.java
@@ -15,3 +15,21 @@ class Solution {
         return func(n-1,nums,dp);
     }
 }
+
+
+// Unique Paths
+class Solution {
+    public int func(int i, int j,int[][] dp) {
+        if(i<0 || j<0) return 0;
+        if(i == 0 && j == 0) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = func(i-1,j,dp) + func(i,j-1,dp);
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return func(m-1,n-1,dp);
+    }
+}
