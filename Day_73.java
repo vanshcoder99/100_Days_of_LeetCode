f356076e75b35bf85be7df7db3436349ce0f1f1b
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-09 16:14:27 +0530

    Add maxProfit method to Solution class
    Implement maxProfit method for stock trading problem.

--- Day_73.java
+++ Day_73.java
@@ -79,3 +79,28 @@ class Solution {
         return func(0,1,fee,prices,dp);
     }
 }
+
+
+
+
+// Best Time to Buy and Sell Stock II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        dp[n][0] = dp[n][1] = 0;
+        int price = 0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                if(buy == 1){
+                    price = Math.max((-1)*prices[i] + dp[i+1][0],0+dp[i+1][1]);
+                }
+                else{
+                    price = Math.max(prices[i] + dp[i+1][1],0+dp[i+1][0]);
+                }
+                dp[i][buy] = price;
+            }
+        }
+        return dp[0][1];
+    }
+}
