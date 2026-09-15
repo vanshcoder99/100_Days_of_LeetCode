4cdce8db9428810495ba62925f3e2deaba817128
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-15 22:11:35 +0530

    Add largestDivisibleSubset method in Solution class
    Implement largest divisible subset algorithm to find the largest subset of numbers where each pair of elements is divisible.

--- Day_78.java
+++ Day_78.java
@@ -72,3 +72,47 @@ class Solution {
        return ans;
     }
 }
+
+
+
+
+
+// 368. Largest Divisible Subset
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<Integer> ans = new ArrayList<>();
+        int[] dp = new int[n];
+        int[] parent = new int[n];
+        
+        for(int i=0;i<n;i++){
+            dp[i] = 1;
+            parent[i] = i;
+        }
+        int midx = -1, mval = -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0){
+                    int val = 1 + dp[j];
+                    if(dp[i] < val){
+                        dp[i] = val;
+                        parent[i] = j;
+                    }
+                }
+            }
+            if(dp[i] > mval){
+                midx = i;
+                mval = dp[i];
+            }
+        }
+
+        
+        while(midx != parent[midx]){
+            ans.add(nums[midx]);
+            midx = parent[midx];
+        }
+        ans.add(nums[midx]);
+        return ans;
+    }
+}
