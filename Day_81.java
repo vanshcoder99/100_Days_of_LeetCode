8ee1c488004e4f2cae70db08b5a79c1d45ae4028
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-22 18:16:28 +0530

    Implement solution for Delete Operation for Two Strings

--- Day_81.java
+++ Day_81.java
@@ -19,3 +19,22 @@ class Solution {
         return prev[m]; 
     }
 }
+
+
+
+
+// 583. Delete Operation for Two Strings
+class Solution {
+    public int func(int i,int j,String w1, String w2,int[][] dp) {
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(w1.charAt(i) == w2.charAt(j)) return dp[i][j] = 1 + func(i-1,j-1,w1,w2,dp);
+        return dp[i][j] = Math.max(func(i-1,j,w1,w2,dp),func(i,j-1,w1,w2,dp));
+    }
+    public int minDistance(String w1, String w2) {
+        int n = w1.length(), m = w2.length();
+        int[][] dp = new int[n][m];
+        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        return ((n + m) - 2*func(n-1,m-1,w1,w2,dp));
+    }
+}
