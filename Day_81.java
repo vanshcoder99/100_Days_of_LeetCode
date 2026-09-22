79b53541a98be1d2b4f22e8cb9293bbcba26ac20
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-22 11:52:36 +0530

    Implement distinct subsequences calculation

---
+++ Day_81.java
@@ -0,0 +1,21 @@
+// 115. Distinct Subsequences
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[] prev = new int[m+1];
+        prev[0] = 1;
+
+        for(int i=1;i<=n;i++){
+            int[] curr = new int[m+1];
+            curr[0] = 1;
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    curr[j] = prev[j-1] + prev[j]; 
+                }
+                else curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[m]; 
+    }
+}
