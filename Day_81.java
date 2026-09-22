5e7ce0187aa333a0236c8e71894d781a3c7aa1ba
Vansh Nagpal <nagpalv931@gmail.com>
2026-09-22 19:09:00 +0530

    Add minimumDeleteSum method to Solution class
    Implement solution for Minimum ASCII Delete Sum problem.

--- Day_81.java
+++ Day_81.java
@@ -38,3 +38,32 @@ class Solution {
         return ((n + m) - 2*func(n-1,m-1,w1,w2,dp));
     }
 }
+
+
+
+
+// 712. Minimum ASCII Delete Sum for Two Strings
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = (int)(s1.charAt(i-1)) + dp[i-1][j-1];
+                }
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int val = dp[n][m], ascii = 0;
+        for(int i=0;i<n;i++){
+            ascii+=(int)(s1.charAt(i));
+        }
+
+        for(int j=0;j<m;j++){
+            ascii+=(int)(s2.charAt(j));
+        }
+        return (ascii-2*val);
+        
+    }
+}
